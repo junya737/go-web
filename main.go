945e2fa007b7c583e71e564fd56914e90d75ba61
ihiratch <junya737@gmail.com>
2025-01-09 16:43:34 +0900

    Print message

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+type PageData struct {
+	Title   string
+	Message string
+}
+
+// ハンドラ関数を定義
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// w: レスポンスを管理する構造体
+	// r: リクエストを保持する構造体
+	// // レスポンスを書き込む
+	// fmt.Fprintln(w, "Hello, World!")
+
+	// テンプレートを使ってHTMLを生成
+	pageData := PageData{
+		Title:   "Hello, World!",
+		Message: "This is a message from the server.",
+	}
+	// テンプレートをパースしてレスポンスに書き込む
+	tmpl, err := template.ParseFiles("template.html")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = tmpl.Execute(w, pageData)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func main() {
+	// "/" パスにリクエストが来たときにhelloHandlerを呼び出す
+	http.HandleFunc("/", helloHandler)
+
+	// サーバーを開始する
+	fmt.Println("Server is running at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
